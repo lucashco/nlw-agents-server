a04bb61d16ca857288eaa3c40ba2d1942bd6030d
Lucas Oliveira <lucascastilholiveira@gmail.com>
2025-07-08 08:03:51 -0300

    feat: add room list

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
+import { getRoomsRoute } from './https/routes/get-rooms.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: 'https://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT }).then();
