924c86fc95cbc92cc11d676ea155625d78785bf9
Lucas Oliveira <lucascastilholiveira@gmail.com>
2025-07-10 12:23:26 -0300

    feat: register uploadAudio route and enable multipart handling

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from '@fastify/cors';
+import { fastifyMultipart } from '@fastify/multipart';
 import { fastify } from 'fastify';
 import {
   serializerCompiler,
@@ -10,6 +11,7 @@ import { createQuestionRoute } from './https/routes/create-question.ts';
 import { createRoomRoute } from './https/routes/create-room.ts';
 import { getRoomsRoute } from './https/routes/get-rooms.ts';
 import { getRoomQuestionsRoute } from './https/routes/get-rooms-questions.ts';
+import { uploadAudioRoute } from './https/routes/upload-audio.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -17,6 +19,7 @@ app.register(fastifyCors, {
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 });
+app.register(fastifyMultipart);
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -25,5 +28,6 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestionsRoute);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT }).then();
